c36a5deba5f3fb6cb46bbf1508c6210997c89726
AkashSDas <aakashdas368@gmail.com>
2021-11-08 19:05:58 +0530

    create course model

---
+++ backend/src/models/course.ts
@@ -0,0 +1,35 @@
+import { Document, model, Schema } from "mongoose";
+import { CourseLevelDocument } from "./course_level";
+import { TeacherDocument } from "./teacher";
+import MongoPaging from "mongo-cursor-pagination";
+
+export type CourseDocument = Document & {
+  teacherId: TeacherDocument;
+  name: string;
+  description: string;
+  ratings: number;
+  numberOfStudentsEnrolled: number;
+  published: boolean;
+  level: CourseLevelDocument;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const CourseSchema = new Schema<CourseDocument>(
+  {
+    teacherId: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    ratings: { type: Number, required: true, default: 0 },
+    numberOfStudentsEnrolled: { type: Number, required: true, default: 0 },
+    published: { type: Boolean, required: true, default: false },
+    level: { type: Schema.Types.ObjectId, ref: "CourseLevel", required: true },
+  },
+  { timestamps: true }
+);
+
+// Pagination
+CourseSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateSchema" });
+
+const Course = model<CourseDocument>("Course", CourseSchema);
+export default Course;
