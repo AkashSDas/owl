2660691dcd111b6295016e6125fc90ead8e9ef78
AkashSDas <aakashdas368@gmail.com>
2021-11-08 14:00:37 +0530

    create expertise model

---
+++ backend/src/models/expertise.ts
@@ -0,0 +1,14 @@
+import { Document, model, Schema } from "mongoose";
+
+export type ExpertiseDocument = Document & {
+  name: string;
+  emoji: string;
+};
+
+const ExpertiseSchema = new Schema<ExpertiseDocument>({
+  name: { type: String, required: true, unqiue: true, trim: true },
+  emoji: { type: String, required: true, trim: true },
+});
+
+const Expertise = model<ExpertiseDocument>("Expertise", ExpertiseSchema);
+export default Expertise;
