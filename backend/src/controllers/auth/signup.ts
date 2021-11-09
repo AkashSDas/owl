92b9b42813158222ffde4b548e9edf7201b43636
AkashSDas <aakashdas368@gmail.com>
2021-11-09 09:48:52 +0530

    create signup controller

---
+++ backend/src/controllers/auth/signup.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import User from "../../models/user";
+import { responseMsg, runAsync } from "../../utils";
+
+/**
+ * @remarks
+ * Save user in the database thus signing up the  user
+ */
+export async function signup(req: Request, res: Response): Promise<void> {
+  const [data, err] = await runAsync(new User(req.body).save());
+  if (err || !data) return responseMsg(res, { status: 400, message: "Signup failed" });
+  return responseMsg(res, { status: 200, error: false, message: "Account created successfully" });
+}
