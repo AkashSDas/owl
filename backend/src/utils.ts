d63e2cea8b79489a9359281c17f06c705ec107be
AkashSDas <aakashdas368@gmail.com>
2021-11-09 09:03:50 +0530

    create ResponseMessage interface and responseMsg util

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,8 +1,11 @@
+import { Response } from "express";
+
 /**
+ * @remarks
  * This will handle async functions to avoid repeating
  * try-catch blocks
  *
- * @returns {[result, err]}
+ * @returns [result, err]
  */
 export async function runAsync(promise: Promise<any>): Promise<Array<any>> {
   try {
@@ -11,4 +14,29 @@ export async function runAsync(promise: Promise<any>): Promise<Array<any>> {
   } catch (err) {
     return [null, err];
   }
+}
+
+/**
+ * @remarks
+ * The response data from all routes will be of the following shape
+ */
+interface ResponseMessage {
+  status: number;
+  error: boolean;
+  message: string;
+  data?: any;
+}
+
+/**
+ * @remarks
+ * Use this to send all the responses from routes
+ * Keeping error=true and next=()=>{} by default
+ */
+export function responseMsg(
+  res: Response,
+  { status, error = true, message, data }: ResponseMessage,
+  next: Function = () => {}
+): void {
+  res.status(status).json({ error, message, data });
+  next();
 }
