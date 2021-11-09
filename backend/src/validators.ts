77bc8066abca9c2d7931a15105277837bfe1943f
AkashSDas <aakashdas368@gmail.com>
2021-11-09 14:49:21 +0530

    create qualification validation

--- backend/src/validators.ts
+++ backend/src/validators.ts
@@ -17,3 +17,10 @@ export const loginValidation = [
   check("email", "Email has wrong format").isEmail(),
   check("password", "Password should be atleast 6 characters").isLength({ min: 6 }),
 ];
+
+export const qualificationValidation = [
+  check("name", "Name is required").exists(),
+  check("emoji", "Emoji is required").exists(),
+  check("name", "Name should be atleast 3 characters").isLength({ min: 3 }),
+  check("emoji", "Emoji should be atmost 4 characters").isLength({ max: 4 }),
+];
