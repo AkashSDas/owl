ab5befedc349149af22d05154ab32a6c96d91b08
AkashSDas <aakashdas368@gmail.com>
2021-11-09 14:15:39 +0530

    add validation middleware in signup route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { login } from "../controllers/auth/login";
 import { logout } from "../controllers/auth/logout";
 import { signup } from "../controllers/auth/signup";
+import { validationCheck } from "../middlewares/express_validation";
 import { loginValidation, signupValidation } from "../validators";
 
 export const router = Router();
 
-router.post("/signup", signupValidation, signup);
+router.post("/signup", signupValidation, validationCheck, signup);
 router.post("/login", loginValidation, login);
 router.get("/logout", logout);
