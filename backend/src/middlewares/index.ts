216c2e8fc7916e477fe91bb8a9346e1d002775cc
AkashSDas <aakashdas368@gmail.com>
2021-11-09 09:29:55 +0530

    create validation check middleware

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { responseMsg } from "../utils";
+
+/**
+ * @remarks
+ * To check validations on req set by express-validator
+ * If no errors then move to next
+ * If any error then send response msg telling about the error
+ */
+export function validationCheck(req: Request, res: Response, next: NextFunction): void {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) return next();
+  return responseMsg(res, { status: 422, message: errors.array()[0].msg });
+}
