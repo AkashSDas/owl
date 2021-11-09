0982a0b284d011470c3e15b2e66e0f0e3f3bd7e1
AkashSDas <aakashdas368@gmail.com>
2021-11-09 12:50:06 +0530

    update signup validation

--- backend/src/validators.ts
+++ backend/src/validators.ts
@@ -1,9 +1,14 @@
 import { check } from "express-validator";
 
 export const signupValidation = [
-  check("email", "Email is required").isEmail(),
-  check("username", "Username should have atleast 3 character").isLength({ min: 3 }),
-  check("password", "Password should have atleast 6 characters").isLength({ min: 6 }),
+  check("email", "Email is required").exists(),
+  check("username", "Username is required").exists(),
+  check("dateOfBirth", "Date of birth is required").exists(),
+  check("password", "Password is required").exists(),
+  check("email", "Email has wrong format").isEmail(),
+  check("dateOfBirth", "Date of birth has wrong format").isDate(),
+  check("username", "Username should be atleast 3 characters").isLength({ min: 3 }),
+  check("password", "Password should be atleast 6 characters").isLength({ min: 6 }),
 ];
 
 export const loginValidation = [
