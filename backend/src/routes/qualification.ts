ec553f72348c1978b69621b965d7ef8201764acc
AkashSDas <aakashdas368@gmail.com>
2021-11-10 11:23:38 +0530

    add qualificationId param and delete route

--- backend/src/routes/qualification.ts
+++ backend/src/routes/qualification.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createQualification } from "../controllers/qualification/create";
+import { deleteQualification } from "../controllers/qualification/delete";
 import { getAllQualifications } from "../controllers/qualification/get_all";
 import { isAdmin, isAuthenticated, isLoggedIn } from "../middlewares/auth";
 import { validationCheck } from "../middlewares/express_validation";
+import { getQualificationById } from "../middlewares/qualification";
 import { getUserById } from "../middlewares/user";
 import { qualificationValidation } from "../validators";
 
@@ -10,6 +12,7 @@ export const router = Router();
 
 // Params
 router.param("userId", getUserById);
+router.param("qualificationId", getQualificationById);
 
 // Routes
 router.post(
@@ -22,3 +25,10 @@ router.post(
   createQualification
 );
 router.get("/", getAllQualifications);
+router.delete(
+  "/:qualificationId/:userId",
+  isLoggedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteQualification
+);
