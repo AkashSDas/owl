e2dad7c6532361689f6c70f55384e8ec35ec0c6d
AkashSDas <aakashdas368@gmail.com>
2021-11-10 11:22:23 +0530

    create get qualification by id middleware

---
+++ backend/src/middlewares/qualification/index.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import Qualification, { QualificationDocument } from "../../models/qualification";
+import { responseMsg, runAsync } from "../../utils";
+
+/**
+ * Get single doc by id
+ */
+export async function getQualificationById(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  qualificationId: string
+) {
+  const [data, err] = await runAsync(Qualification.findById(qualificationId).exec());
+  if (err)
+    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
+  if (!data) return responseMsg(res, { status: 400, message: "Qualification doesn't exists" });
+
+  const qualification: QualificationDocument = data;
+  req.qualification = qualification;
+
+  next();
+}
