4fd3d291ef9766f4ea27d9982045ebfffa474ef7
AkashSDas <aakashdas368@gmail.com>
2021-11-11 11:34:54 +0530

    add courseId param and create update couser public metadata route

--- backend/src/routes/course.ts
+++ backend/src/routes/course.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createCourse } from "../controllers/course/create";
+import { updateCoursePublicMetadata } from "../controllers/course/update";
 import { isAuthenticated, isLoggedIn, isTeacher } from "../middlewares/auth";
+import { getCourseById } from "../middlewares/course";
 import { validationCheck } from "../middlewares/express_validation";
 import { getUserById } from "../middlewares/user";
 import { courseCreateValidation } from "../validators";
@@ -9,6 +11,7 @@ export const router = Router();
 
 // Params
 router.param("userId", getUserById);
+router.param("courseId", getCourseById);
 
 // Routes
 router.post(
@@ -19,4 +22,11 @@ router.post(
   courseCreateValidation,
   validationCheck,
   createCourse
+);
+router.put(
+  "/:courseId/:userId",
+  isLoggedIn,
+  isAuthenticated,
+  isTeacher,
+  updateCoursePublicMetadata
 );
