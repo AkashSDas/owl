a4c1c9345c05165df24836ea25a28c64fb0f410e
AkashSDas <aakashdas368@gmail.com>
2021-11-12 12:25:38 +0530

    create delete chapter controller and route for it

--- backend/src/controllers/chapter.ts
+++ backend/src/controllers/chapter.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import Chapter from "../models/chapter";
+import Course from "../models/course";
 import CourseChapters from "../models/course_chapters";
-import { LessonDocument } from "../models/lesson";
+import Lesson from "../models/lesson";
 import { responseMsg, runAsync } from "../utils";
+import { bucket } from "../firebase";
 
 /**
  * Create a chapter and then add it to course chapters list
@@ -40,11 +42,71 @@ export async function updateChapter(req: Request, res: Response) {
   const { name, description } = req.body;
   if (name) chapter.name = name;
   if (description) chapter.description = description;
-  const [data, err] = await runAsync(chapter.save());
+  const [, err] = await runAsync(chapter.save());
   if (err) return responseMsg(res, { status: 400, message: "Failed to update chapter" });
   return responseMsg(res, {
     status: 200,
     error: false,
     message: "Successfully updated chapter",
+  });
+}
+
+/**
+ * Delete a chapter
+ *
+ * @remarks
+ * While deleting a chapter there're multiple work
+ * - Delete individual lessons (their mongoDB doc, firebase video)
+ * - Update lesson number and course duration in course doc
+ * - Remove this chater from chapter courses list
+ */
+export async function deleteChapter(req: Request, res: Response) {
+  const courseChapters = req.courseChapters;
+  const chapter = req.chapter;
+
+  // Get total chapter video duration
+  let chapterDuration = 0;
+  const [lessons, _] = await runAsync(
+    Lesson.find({ _id: { $in: chapter.lessons } }, "videoDuration").exec()
+  );
+  lessons.filter((doc) => {
+    chapterDuration = doc.videoDuration + chapterDuration;
+  });
+
+  // Delete all lessons
+  await runAsync(Lesson.deleteMany({ _id: { $in: chapter.lessons } }).exec());
+  // Delete videos from firebase
+  for (const doc of lessons) {
+    await runAsync(bucket.deleteFiles({ prefix: `lesson-videos/${doc._id}` }));
+  }
+
+  // Update course
+  await runAsync(
+    Course.updateOne(
+      { _id: courseChapters.courseId },
+      {
+        $inc: {
+          courseLength: -chapterDuration,
+          numberOfLectures: -lessons.length,
+        },
+      }
+    ).exec()
+  );
+
+  // Remove this chapter from course chapters list
+  const chapters = courseChapters.chapters.filter((id) => {
+    if (id.equals(chapter._id)) return false;
+    return true;
+  });
+  courseChapters.chapters = chapters;
+  await runAsync(courseChapters.save());
+
+  // Delete chapter
+  await runAsync(chapter.deleteOne({ _id: chapter._id }));
+
+  return responseMsg(res, {
+    status: 200,
+    error: false,
+    message: "Successfully deleted chapter",
   });
 }
--- backend/src/routes/chapter.ts
+++ backend/src/routes/chapter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createChapterAndPushToCourseChapters, updateChapter } from "../controllers/chapter";
+import {
+  createChapterAndPushToCourseChapters,
+  deleteChapter,
+  updateChapter,
+} from "../controllers/chapter";
 import { isAuthenticated, isLoggedIn, isTeacher } from "../middlewares/auth";
 import { getCourseChaptersByCourseId } from "../middlewares/course_chapters";
 import { getUserById } from "../middlewares/user";
@@ -21,3 +25,10 @@ router.post(
   createChapterAndPushToCourseChapters
 );
 router.put("/:chapterId/:userId", isLoggedIn, isAuthenticated, isTeacher, updateChapter);
+router.delete(
+  "/:chapterId/:courseId/:userId",
+  isLoggedIn,
+  isAuthenticated,
+  isTeacher,
+  deleteChapter
+);
