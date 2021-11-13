27895dd931c176d51c8b7330ed6de838db7668b8
AkashSDas <aakashdas368@gmail.com>
2021-11-13 10:12:31 +0530

    rm lessons field and add courseId field and document chapter model

--- backend/src/models/chapter.ts
+++ backend/src/models/chapter.ts
@@ -1,17 +1,26 @@
 import { Document, model, Schema } from "mongoose";
-import { LessonDocument } from "./lesson";
+import { CourseDocument } from "./course";
+
+/**
+ * Shape of a chapter. This will keep info about a chapter and its id will be used
+ * in the lesson doc which will belong to this chapter
+ *
+ * This doc will have one-to-many relation with the course.
+ * A lesson will have many-to-one relation (many lessons for one chapter)
+ *
+ * @todo
+ * - Add a way to re-order lessons in a chapter
+ * - Add a way to re-order chapters in a course
+ */
 
 export type ChapterDocument = Document & {
-  lessons: LessonDocument[];
+  courseId: CourseDocument;
   name: string;
   description: string;
 };
 
 const ChapterSchema = new Schema<ChapterDocument>({
-  lessons: {
-    type: [{ type: Schema.Types.ObjectId, ref: "Lesson" }],
-    default: [],
-  },
+  courseId: { type: Schema.Types.ObjectId, ref: "Course", required: true },
   name: { type: String, required: true, trim: true },
   description: { type: String, required: true, trim: true },
 });
