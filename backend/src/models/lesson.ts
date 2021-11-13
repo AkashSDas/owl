4d5c9c773ef3c308a7c41d05df2c9ccb0e36125d
AkashSDas <aakashdas368@gmail.com>
2021-11-13 10:12:51 +0530

    add courseId and chapterId fields in lesson doc and document it

--- backend/src/models/lesson.ts
+++ backend/src/models/lesson.ts
@@ -1,6 +1,19 @@
 import { Document, model, Schema } from "mongoose";
+import { ChapterDocument } from "./chapter";
+import { CourseDocument } from "./course";
+
+/**
+ * Shape of a lesson
+ * Lesson has one-to-many relation with course and chapter. Having courseId and chapterId
+ * in lesson doc simplifies a lot of querying
+ *
+ * Fields like course length (duration) and number of lectures (lessons) can be optained
+ * by simple query on lesson model since it has couseId field in it
+ */
 
 export type LessonDocument = Document & {
+  courseId: CourseDocument;
+  chapterId: ChapterDocument;
   name: string;
   description: string;
   note?: string;
@@ -9,6 +22,8 @@ export type LessonDocument = Document & {
 };
 
 const LessonSchema = new Schema<LessonDocument>({
+  courseId: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+  chapterId: { type: Schema.Types.ObjectId, ref: "Chapter", required: true },
   name: { type: String, required: true, trim: true },
   description: { type: String, required: true, trim: true },
   note: { type: String, trim: true },
