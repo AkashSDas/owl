8be2befb1b4094db64ec46fe50c505f5c0be2a5f
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:20:11 +0530

    refactor and document teacher middleware

--- backend/src/middlewares/teacher.ts
+++ backend/src/middlewares/teacher.ts
@@ -1,20 +1,17 @@
-import { NextFunction, Request, Response } from "express";
 import Teacher, { TeacherDocument } from "../models/teacher";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
-export async function getTeacherById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  teacherId: string
-) {
-  const [data, err] = await runAsync(Teacher.findById(teacherId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Teacher doesn't exists" });
-
+/**
+ * Get teacher (if exists) and set it to req.teacher
+ *
+ * @params
+ * id: teacher mongodb id
+ */
+export const getTeacherById: IdMiddleware = async (req, res, next, id) => {
+  const [data, err] = await runAsync(Teacher.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data) return responseMsg(res, { msg: "Teacher doesn't exists" });
   const teacher: TeacherDocument = data;
   req.teacher = teacher;
-
   next();
-}
+};
