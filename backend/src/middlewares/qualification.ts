afd0b6ba20158620494425ca34d2ce914c5688e4
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:00:43 +0530

    refactor qualification middleware

--- backend/src/middlewares/qualification.ts
+++ backend/src/middlewares/qualification.ts
@@ -1,23 +1,23 @@
-import { NextFunction, Request, Response } from "express";
 import Qualification, { QualificationDocument } from "../models/qualification";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
 /**
- * Get single doc by id
+ * Get qualifiction (if exists) and set it to req.qualification
+ *
+ * @params
+ * id: qualification mongodb id
  */
-export async function getQualificationById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  qualificationId: string
-) {
-  const [data, err] = await runAsync(Qualification.findById(qualificationId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Qualification doesn't exists" });
-
-  const qualification: QualificationDocument = data;
-  req.qualification = qualification;
-
+export const getQualificationById: IdMiddleware = async (
+  req,
+  res,
+  next,
+  id
+) => {
+  const [data, err] = await runAsync(Qualification.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data)
+    return responseMsg(res, { msg: "Qualification doesn't exists" });
+  const q: QualificationDocument = data;
+  req.qualification = q;
   next();
-}
+};
