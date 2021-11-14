97fd751cabc7961d25966f3a06daf8945f96d940
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:15:40 +0530

    refactor course level middleware

--- backend/src/middlewares/course_level.ts
+++ backend/src/middlewares/course_level.ts
@@ -1,23 +1,18 @@
-import { NextFunction, Request, Response } from "express";
 import CourseLevel, { CourseLevelDocument } from "../models/course_level";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
 /**
- * Get single doc by id
+ * Get course level (if exists) and set it to req.courseLevel
+ *
+ * @params
+ * id: course level mongodb id
  */
-export async function getCourseLevelById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  courseLevelId: string
-) {
-  const [data, err] = await runAsync(CourseLevel.findById(courseLevelId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Course level doesn't exists" });
-
-  const courseLevel: CourseLevelDocument = data;
-  req.courseLevel = courseLevel;
-
+export const getCourseLevelById: IdMiddleware = async (req, res, next, id) => {
+  const [data, err] = await runAsync(CourseLevel.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data)
+    return responseMsg(res, { msg: "Course level doesn't exists" });
+  const lvl: CourseLevelDocument = data;
+  req.courseLevel = lvl;
   next();
-}
+};
