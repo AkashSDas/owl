b935112d81a67552185cba8bf2c521d860c60cfa
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:22:10 +0530

    refactor and document course controller

--- backend/src/controllers/course.ts
+++ backend/src/controllers/course.ts
@@ -1,226 +1,120 @@
-import { Request, Response } from "express";
 import Course from "../models/course";
-import CourseChapters from "../models/course_chapters";
-import { responseMsg, runAsync } from "../utils";
-import formidable from "formidable";
-import _ from "lodash";
-import { v4 } from "uuid";
-import { bucket } from "../firebase";
-import Lesson from "../models/lesson";
+import { Controller, responseMsg, responseMsgs, runAsync } from "../utils";
+import { IncomingForm } from "formidable";
+import { courseUpdateFormCallback } from "../helpers/course";
 import Chapter from "../models/chapter";
+import Lesson from "../models/lesson";
+import { deleteFileInFirebaseStorage } from "../firebase";
 
 /**
  * Create course
  *
  * @remarks
- * req.body shape will be
- *  {
- *      name,
- *      description,
- *      level
- *  }
  *
- * Course will be created incrementally so therefore only using
- * name, description and level (course level) to create the course
+ * Shape of req.body will be
+ * - name
+ * - description
+ * - level - This is course level (mongoId)
+ * - price
  *
- * @todo
- * - Make creation of course and course chapters robust so that if course is created
- * later on course chapters can be created separately
+ * Other value have default values out which few fields can be updated
+ * directly by the user and other shouldn't be updated the users but will
+ * be updated by different processes in the backend
+ *
+ * This controller should be used in conjunction with
+ * - auth middlewares
+ * - isTeacher middlewares to see if the requested user is a teacher or not
+ * - getTeacherById middleware which will set req.teacher which will used for teacherId
  */
-export async function createCourse(req: Request, res: Response) {
-  const user = req.profile; // since teacherId === user._id
-
-  // Only below values will be used to create a course
-  const { name, description, level } = req.body;
-  const courseData = {
-    teacherId: user._id,
-    name,
-    description,
-    level,
-  };
-
-  const [data, err] = await runAsync(new Course(courseData).save());
-
-  if (err || !data)
-    return responseMsg(res, {
-      status: 400,
-      message: "Failed to create course",
-    });
-
-  // Create course chapters
-  const [chapters, error] = await runAsync(new CourseChapters({ courseId: data._id }).save());
-  if (error || !chapters)
-    return responseMsg(res, {
-      status: 400,
-      message: "Failed to create course",
-    });
-
+export const createCourse: Controller = async (req, res) => {
+  const [course, err] = await runAsync(
+    new Course({ teacherId: req.teacher._id, ...req.body }).save()
+  );
+  if (err || !course) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
   return responseMsg(res, {
     status: 200,
     error: false,
-    message: "Course created successfully",
+    msg: "Successfully created the course",
+    data: { course },
   });
-}
+};
 
 /**
- * Update course metadata that are meant to be updated by user (teacher)
+ * Update course
  *
  * @remarks
- * course metadata that are public
- *  - name
- *  - description
- *  - coverImgURL
- *  - level
- * Shape of req.body will be (note all of these fields are optional)
- *  {
- *      name,           string
- *      description,    string
- *      coverImg,       file
- *      level           mongoId
- *  }
+ *
+ * This controller should be used in conjunction with
+ * - getCourseById middleware which will set the `req.course`
+ *
+ * Shape of req.body (with all fields being optional)
+ * - name
+ * - description
+ * - price
+ * - level
+ * - coverImg (this is a img file, this will be updated with course
+ * cover img and then coverImgURL will be updated)
+ *
+ * Only the specified fields in Course model should be updated by user's
+ * and others are updated by different proccess in the backend
+ * - name
+ * - description
+ * - price
+ * - level
+ * - updatedAt
+ *
+ * The **updatedAt** is a custom updatedAt field which should be updated if
+ * name, description, price, level or coverImgURL, any of these fields are
+ * updated then. Also note that updatedAt should also be updated whenever
+ * a chapter/lesson is created/updated/deleted
  *
  * @todo
- * - Add check on incoming data i.e. fields - it shouldn't contain
- * any other fields to avoid accidental update of any other field in course doc
+ * - Add filter that only the allowed fields are updated
  */
-export async function updateCoursePublicMetadata(req: Request, res: Response) {
-  let form = new formidable.IncomingForm({ keepExtensions: true });
-
-  const filterData = (fields: formidable.Fields): Object => {
-    let newData = {};
-
-    // only these are going to be present in `fields`
-    const updateFields = ["name", "description", "level"];
-
-    for (const key in fields) {
-      if (updateFields.filter((field) => field === key).length === 1) {
-        newData[key] = fields[key];
-      }
-    }
-
-    return newData;
-  };
-
-  form.parse(req, async (err: any, fields: formidable.Fields, files: formidable.Files) => {
-    if (err) {
-      // If user is coming till here after passing all vaidators
-      // then chances are high that the problem is in file itself
-      return responseMsg(res, {
-        status: 400,
-        message: "There is some problem with the image file",
-      });
-    }
-
-    let course = req.course;
-    const updateData = filterData(fields);
-    course = _.extend(course, updateData);
-
-    const { coverImg } = files;
-    if (coverImg) {
-      // Save cover img
-      const courseId = req.course._id;
-      const filename = (coverImg as formidable.File).originalFilename;
-
-      const uuid = v4();
-      const destination = `course-cover-imgs/${courseId}/${filename}`;
-
-      // Delete old cover img
-      // Note: Be careful with deleteFiles, if empty string is passed to prefix
-      // then it will delete everything in the bucket
-      const [, deleteErr] = await runAsync(
-        bucket.deleteFiles({ prefix: `course-cover-imgs/${courseId}` })
-      );
-
-      // If there's only one file to be deleted then use the below method
-      //   const [, deleteErr] = await runAsync(
-      //     bucket.file(`blog-post-cover-imgs/${userId}/${blogPost._id}/${filename}`).delete()
-      //   );
-
-      if (deleteErr)
-        return responseMsg(res, { status: 400, message: "Failed to delete post cover image" });
-
-      const [, err] = await runAsync(
-        bucket.upload((coverImg as formidable.File).filepath, {
-          destination,
-          metadata: { contentType: "image/png", metadata: { firebaseStorageDownloadTokens: uuid } },
-        })
-      );
-
-      if (err) return responseMsg(res, { status: 400, message: "Could not save cover image" });
-
-      const photoURL =
-        "https://firebasestorage.googleapis.com/v0/b/" +
-        bucket.name +
-        "/o/" +
-        encodeURIComponent(destination) +
-        "?alt=media&token=" +
-        uuid;
-
-      course.coverImgURL = photoURL;
-    }
-
-    const [savedCourse, error] = await runAsync(course.save());
-    if (error) return responseMsg(res, { status: 400, message: "Could not update course" });
-    return responseMsg(res, {
-      status: 200,
-      error: false,
-      message: "Successfully update course",
-      data: {
-        // Just sending fields that were updated instead of the entire payload
-        name: savedCourse.name,
-        description: savedCourse.description,
-        level: savedCourse.level,
-        coverImgURL: savedCourse.coverImgURL,
-      },
-    });
-  });
-}
+export const updateCoursePublicData: Controller = async (req, res) => {
+  let form = new IncomingForm({ keepExtensions: true });
+  form.parse(req, (err, fields, files) =>
+    courseUpdateFormCallback(req, res, err, fields, files)
+  );
+};
 
 /**
  * Delete a course
  *
  * @remarks
- * Deleting a course involves multiple work
- * - Deleting course doc
- * - Deleting courseChapters doc where courseId === course._id
- * - Deleting all chapters in courseChapters
- * - Deleting all lessons in above chapters
- * - Delete video files of above lessons in firebase
- * One good thing is that we don't have to update any metadata as they all are going to be deleted
+ * Use this controller in conjunction with getCourseById which will set `req.course`
+ *
+ * @todo
+ * - Instead of getting entire course just get the course id with validation check if
+ * its there or not
  */
-export async function deleteCourse(req: Request, res: Response) {
+export const deleteCourse: Controller = async (req, res) => {
   const course = req.course;
 
-  // Get course chapters
-  const [courseChapters, _err1] = await runAsync(
-    CourseChapters.findOne({ courseId: course._id }).populate("chapters", "lessons").exec()
+  // Delete all videos of lessons in this course
+  const destination = `lesson-videos/${course._id}`;
+  const wasDeleted = await deleteFileInFirebaseStorage(destination);
+  if (!wasDeleted) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+
+  // Delete all lessons in this course
+  const [, err1] = await runAsync(
+    Lesson.deleteMany({ courseId: course._id }).exec()
   );
+  if (err1) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
-  // Needed to delete all chapters and lessons associated with this
-  let chapterIds = [];
-  let lessonIds = [];
+  // Delete all chapters in this course
+  const [, err2] = await runAsync(
+    Chapter.deleteMany({ courseId: course._id }).exec()
+  );
+  if (err2) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
-  // Deleting video files of all lessons
-  for (const chapter of courseChapters.chapters) {
-    chapterIds.push(chapter._id);
-
-    for (const lessonId of chapter.lessons) {
-      lessonIds.push(lessonId);
-      await runAsync(bucket.deleteFiles({ prefix: `lesson-videos/${lessonId}` }));
-    }
-  }
-
-  // Delete lessons and chapters
-  await runAsync(Chapter.deleteMany({ _id: { $in: chapterIds } }).exec());
-  await runAsync(Lesson.deleteMany({ _id: { $in: lessonIds } }).exec());
-
-  // Delete course chapters and course
-  await runAsync(CourseChapters.deleteOne({ courseId: course._id }).exec());
-  await runAsync(Course.deleteOne({ _id: course._id }).exec());
+  // Delete course doc
+  const [, err3] = await runAsync(Course.deleteOne({ _id: course._id }).exec());
+  if (err3) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
   return responseMsg(res, {
     status: 200,
     error: false,
-    message: "Successfully deleted the course",
+    msg: "Successfully delete the course",
   });
-}
+};
