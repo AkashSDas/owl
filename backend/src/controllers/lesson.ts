23efe6e1762f534d82e58cfcb6f55e81b8af47c6
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:19:45 +0530

    refactor and document lesson controller

--- backend/src/controllers/lesson.ts
+++ backend/src/controllers/lesson.ts
@@ -1,208 +1,83 @@
-import { Request, Response } from "express";
-import { Fields, File, Files, IncomingForm } from "formidable";
-import { v4 } from "uuid";
-import { bucket } from "../firebase";
-import Lesson from "../models/lesson";
-import { responseMsg, runAsync } from "../utils";
-import { getVideoDurationInSeconds } from "get-video-duration";
-import _ from "lodash";
+/**
+ * @todo
+ * - Check whether the chapter and course in req.params exists or not
+ * - Check whether the chapter is of course or not
+ */
+
+import { IncomingForm } from "formidable";
+import { deleteFileInFirebaseStorage } from "../firebase";
+import { Controller, responseMsg, responseMsgs, runAsync } from "../utils";
+import {
+  lessonCreateFormCallback,
+  lessonUpdateFormCallback,
+} from "../helpers/lesson";
 
 /**
+ * Create a lesson
+ *
  * @remarks
- * Create lesson then update chapter's lesson list and then increment lessons number in course along with course length
- * Shape of req.body
- * {
- *  name,           string
- *  description,    string
- *  note,           string (markdown) (optional)
- *  video,          file (video file)
- * }
+ *
+ * Shape of req.body should be
+ * - name
+ * - description
+ * - note (optional, this will be markdown text)
+ * - video (file which will saved to firebase storage and then url will be saved in the doc)
+ *
+ * req.params should have the following
+ * - courseId
+ * - chapterId
  */
-export async function createLessonAndPushToChapter(req: Request, res: Response) {
+export const createLesson: Controller = async (req, res) => {
   let form = new IncomingForm({ keepExtensions: true });
-
-  form.parse(req, async (err: any, fields: Fields, files: Files) => {
-    if (err) {
-      // If user is coming till here after passing all vaidators
-      // then chances are high that the problem is in file itself
-      return responseMsg(res, {
-        status: 400,
-        message: "There is some problem with the video file",
-      });
-    }
-
-    let [lesson, error] = await runAsync(new Lesson(fields).save());
-    if (error || !lesson)
-      return responseMsg(res, {
-        status: 400,
-        message: "Something went wrong, Please try again",
-      });
-
-    const { video } = files;
-    const filename = (video as File).originalFilename;
-    const uuid = v4();
-    const destination = `lesson-videos/${lesson._id}/${filename}`;
-    const [, e] = await runAsync(
-      bucket.upload((video as File).filepath, {
-        destination,
-        metadata: { metadata: { firebaseStorageDownloadTokens: uuid } },
-      })
-    );
-    if (e) return responseMsg(res, { status: 400, message: "Failed to upload video" });
-    const videoURL =
-      "https://firebasestorage.googleapis.com/v0/b/" +
-      bucket.name +
-      "/o/" +
-      encodeURIComponent(destination) +
-      "?alt=media&token=" +
-      uuid;
-
-    const duration = parseFloat(((await getVideoDurationInSeconds(videoURL)) / 60).toFixed(2));
-    lesson.videoURL = videoURL;
-    lesson.videoDuration = duration;
-
-    const [savedLesson, err2] = await runAsync(lesson.save());
-    if (err2)
-      return responseMsg(res, {
-        status: 400,
-        message: "Failed to save video url, Please try again",
-      });
-
-    // increment lessons count
-    const course = req.course;
-    course.numberOfLectures = course.numberOfLectures + 1;
-    course.courseLength = course.courseLength + duration;
-    await runAsync(course.save());
-
-    // add to chapters list
-    const chapter = req.chapter;
-    chapter.lessons.push(lesson._id);
-    await runAsync(chapter.save());
-
-    return responseMsg(res, {
-      status: 200,
-      error: false,
-      message: "Successfully created lesson",
-      data: { lesson: savedLesson },
-    });
-  });
-}
+  form.parse(req, (err, fields, files) =>
+    lessonCreateFormCallback(req, res, err, fields, files)
+  );
+};
 
 /**
- * @remarks
- * Here fields can have name, description, note (all of them being optional)
+ * Update a lesson
+ *
+ * req.params should have the following
+ * - courseId
+ * - chapterId
  */
-export async function updateLesson(req: Request, res: Response) {
+export const updateLesson: Controller = async (req, res) => {
   let form = new IncomingForm({ keepExtensions: true });
+  form.parse(req, (err, fields, files) =>
+    lessonUpdateFormCallback(req, res, err, fields, files)
+  );
+};
 
-  form.parse(req, async (err: any, fields: Fields, files: Files) => {
-    if (err) {
-      // If user is coming till here after passing all vaidators
-      // then chances are high that the problem is in file itself
-      return responseMsg(res, {
-        status: 400,
-        message: "There is some problem with the video file",
-      });
-    }
-
-    let lesson = req.lesson;
-    lesson = _.extend(lesson, fields);
-
-    const { video } = files;
-    if (video) {
-      // Updating video in firebase bucket
-
-      const filename = (video as File).originalFilename;
-      const uuid = v4();
-      const destination = `lesson-videos/${lesson._id}/${filename}`;
-      const oldVideoDuration = parseFloat(
-        ((await getVideoDurationInSeconds(lesson.videoURL)) / 60).toFixed(2)
-      );
-
-      // Delete video
-      // Note: Be careful with deleteFiles, if empty string is passed to prefix
-      // then it will delete everything in the bucket
-      const [, deleteErr] = await runAsync(
-        bucket.deleteFiles({ prefix: `lesson-videos/${lesson._id}` })
-      );
-
-      // If there's only one file to be deleted then use the below method
-      //   const [, deleteErr] = await runAsync(
-      //     bucket.file(`lesson-videos/${lesson._id}/${filename}`).delete()
-      //   );
-
-      if (deleteErr)
-        return responseMsg(res, { status: 400, message: "Failed to delete lesson video" });
-
-      // Adding the new video
-      const [, e] = await runAsync(
-        bucket.upload((video as File).filepath, {
-          destination,
-          metadata: { metadata: { firebaseStorageDownloadTokens: uuid } },
-        })
-      );
-      if (e) return responseMsg(res, { status: 400, message: "Failed to upload video" });
-      const videoURL =
-        "https://firebasestorage.googleapis.com/v0/b/" +
-        bucket.name +
-        "/o/" +
-        encodeURIComponent(destination) +
-        "?alt=media&token=" +
-        uuid;
-
-      const duration = parseFloat(((await getVideoDurationInSeconds(videoURL)) / 60).toFixed(2));
-      lesson.videoURL = videoURL;
-      lesson.videoDuration = duration;
-
-      // Updating course duration
-      const course = req.course;
-      course.courseLength = course.courseLength - oldVideoDuration + duration;
-      await runAsync(course.save());
-    }
-
-    const [savedLesson, err2] = await runAsync(lesson.save());
-    if (err2) return responseMsg(res, { status: 400, message: "Failed to update course" });
-
-    return responseMsg(res, {
-      status: 200,
-      error: false,
-      message: "Sccessfully updated the course",
-      data: { lesson: savedLesson },
-    });
-  });
-}
-
-export async function deleteLesson(req: Request, res: Response) {
-  // All the below docs are going to be updated (lesson will be deleted)
+/**
+ * Delete a lesson
+ *
+ * @remarks
+ *
+ * req.params should have the following
+ * - courseId
+ * - chapterId
+ *
+ * Use this controller in conjunction with getLessonById which will set `req.lesson`
+ *
+ * @todo
+ * - Instead of getting entire lesson just get the lesson id with validation check of
+ * that lesson existing and having belonging to course and chapter whose ids are given
+ * in param
+ */
+export const deleteLesson: Controller = async (req, res) => {
   const lesson = req.lesson;
-  const course = req.course;
-  const chapter = req.chapter;
 
-  // Get video duration
-  const duration = lesson.videoDuration;
+  // Delete lesson video
+  const destination = `lesson-videos/${req.params.courseId}/${req.params.chapterId}/${lesson._id}`;
+  const wasDeleted = await deleteFileInFirebaseStorage(destination);
+  if (!wasDeleted) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
-  // Delete video
-  const [, err1] = await runAsync(bucket.deleteFiles({ prefix: `lesson-videos/${lesson._id}` }));
-  if (err1) return responseMsg(res, { status: 400, message: "Failed to delete lesson video" });
-
-  // Delete lesson doc
-  const [, err2] = await runAsync(lesson.deleteOne({ _id: lesson._id }));
-  if (err2) return responseMsg(res, { status: 400, message: "Failed to delete lesson" });
-
-  // Update chapter lessons
-  chapter.lessons = chapter.lessons.filter((lessonId) => {
-    if (lessonId.equals(lesson._id)) return false;
-    return true;
+  // Delete the doc
+  const [, err] = await runAsync(lesson.deleteOne({ _id: lesson._id }).exec());
+  if (!err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  return responseMsg(res, {
+    status: 200,
+    error: false,
+    msg: "Successfully deleted the lesson",
   });
-  const [, err3] = await runAsync(chapter.save());
-  if (err3)
-    return responseMsg(res, { status: 400, message: "Failed to update chapter lessons list" });
-
-  // Update course duration
-  course.courseLength = course.courseLength - duration;
-  course.numberOfLectures = course.numberOfLectures - 1;
-  const [, err4] = await runAsync(course.save());
-  if (err4) return responseMsg(res, { status: 400, message: "Failed to update course duration" });
-
-  return responseMsg(res, { status: 200, error: false, message: "Successfully deleted lesson" });
-}
+};
