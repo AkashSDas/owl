5becadb3a584c65996175295b20a5c00e4d8634f
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:20:50 +0530

    refactor and document chapter controller

--- backend/src/controllers/chapter.ts
+++ backend/src/controllers/chapter.ts
@@ -1,112 +1,119 @@
-import { Request, Response } from "express";
+/**
+ * @todo
+ * - Check whether the chapter on whom the operation are being performed
+ * belong to the course whose id is given
+ */
+
 import Chapter from "../models/chapter";
 import Course from "../models/course";
-import CourseChapters from "../models/course_chapters";
+import { Controller, responseMsg, responseMsgs, runAsync } from "../utils";
+import { extend } from "lodash";
 import Lesson from "../models/lesson";
-import { responseMsg, runAsync } from "../utils";
-import { bucket } from "../firebase";
+import { deleteFileInFirebaseStorage } from "../firebase";
 
 /**
- * Create a chapter and then add it to course chapters list
+ * Create a chapter
  *
  * @remarks
- * Here req.body will be of shape {name, description}
+ *
+ * Shape of req.body is going to be
+ * - name
+ * - description
  */
-export async function createChapterAndPushToCourseChapters(req: Request, res: Response) {
-  const courseChapters = req.courseChapters;
-  const [chapter, err1] = await runAsync(new Chapter(req.body).save());
-  if (err1) return responseMsg(res, { status: 400, message: "Failed to create chapter" });
+export const createChapter: Controller = async (req, res) => {
+  const courseId = req.params.courseId;
 
-  const [, err2] = await runAsync(
-    CourseChapters.findOneAndUpdate(
-      { _id: courseChapters._id },
-      { $push: { chapters: chapter._id } }
-    ).exec()
+  // Check whether the course exists or not
+  const [exists, err1] = await runAsync(
+    Course.findOne({ _id: courseId }, "_id").exec()
   );
-  if (err2) return responseMsg(res, { status: 400, message: "Failed to create chapter" });
+  if (err1) return responseMsg(err1, { msg: responseMsgs.WENT_WRONG });
+  if (!exists) return responseMsg(err1, { msg: "Course doesn't exists" });
 
+  // Create chapter for the given course
+  const [chapter, err2] = await runAsync(
+    new Chapter({ courseId, ...req.body }).save()
+  );
+  if (err2 || !chapter)
+    return responseMsg(err1, { msg: responseMsgs.WENT_WRONG });
   return responseMsg(res, {
     status: 200,
     error: false,
-    message: "Successfully created chapter",
+    msg: "Successfully created chapter",
     data: { chapter },
   });
-}
+};
 
 /**
- * This update controller won't update lessons as they
- * are meant to be updated when lessons are created or deleted
+ * Update a chapter
+ *
+ * @remarks
+ *
+ * Only thing that can be updated in a chapter is
+ * - name
+ * - description
+ * courseId is not meant to be updated as of now
+ *
+ * Use this controller in conjunction with
+ * - getChapterById which will set `req.chapter`
  */
-export async function updateChapter(req: Request, res: Response) {
-  const chapter = req.chapter;
-  const { name, description } = req.body;
-  if (name) chapter.name = name;
-  if (description) chapter.description = description;
-  const [, err] = await runAsync(chapter.save());
-  if (err) return responseMsg(res, { status: 400, message: "Failed to update chapter" });
+export const updateChapter: Controller = async (req, res) => {
+  let chapter = req.chapter;
+  chapter = extend(chapter, req.body);
+  const [updatedChapter, err] = await runAsync(chapter.save());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
   return responseMsg(res, {
     status: 200,
     error: false,
-    message: "Successfully updated chapter",
+    msg: "Successfully updated the chapter",
+    data: { chapter: updatedChapter },
   });
-}
+};
 
 /**
  * Delete a chapter
  *
  * @remarks
- * While deleting a chapter there're multiple work
- * - Delete individual lessons (their mongoDB doc, firebase video)
- * - Update lesson number and course duration in course doc
- * - Remove this chater from chapter courses list
+ *
+ * req.params should have the following
+ * - courseId
+ *
+ * Use this controller in conjunction with getChapterById which will set `req.chapter`
+ *
+ * @todo
+ * - Instead of getting entire chapter just get the chapter id with validation check of
+ * that chapter existing and having belonging to course whose id is given in param
  */
-export async function deleteChapter(req: Request, res: Response) {
-  const courseChapters = req.courseChapters;
+export const deleteChapter: Controller = async (req, res) => {
   const chapter = req.chapter;
 
-  // Get total chapter video duration
-  let chapterDuration = 0;
-  const [lessons, _] = await runAsync(
-    Lesson.find({ _id: { $in: chapter.lessons } }, "videoDuration").exec()
+  /**
+   * Delete all lessons in this chapter
+   */
+
+  // Delete all videos
+  // This will delete all the lesson videos in a chapter
+  const destination = `lesson-videos/${req.params.courseId}/${chapter._id}`;
+  const wasDeleted = await deleteFileInFirebaseStorage(destination);
+  if (!wasDeleted) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+
+  // Delete all lesson docs
+  const [, err2] = await runAsync(
+    Lesson.deleteMany({ chapterId: chapter._id }).exec()
   );
-  lessons.filter((doc) => {
-    chapterDuration = doc.videoDuration + chapterDuration;
-  });
+  if (err2) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
-  // Delete all lessons
-  await runAsync(Lesson.deleteMany({ _id: { $in: chapter.lessons } }).exec());
-  // Delete videos from firebase
-  for (const doc of lessons) {
-    await runAsync(bucket.deleteFiles({ prefix: `lesson-videos/${doc._id}` }));
-  }
-
-  // Update course
-  await runAsync(
-    Course.updateOne(
-      { _id: courseChapters.courseId },
-      {
-        $inc: {
-          courseLength: -chapterDuration,
-          numberOfLectures: -lessons.length,
-        },
-      }
-    ).exec()
+  /**
+   * Delete the chapter doc
+   */
+  const [, err3] = await runAsync(
+    Chapter.deleteOne({ _id: chapter._id }).exec()
   );
-
-  // Remove this chapter from course chapters list
-  const chapters = courseChapters.chapters.filter((id) => {
-    if (id.equals(chapter._id)) return false;
-    return true;
-  });
-  courseChapters.chapters = chapters;
-  await runAsync(courseChapters.save());
-
-  // Delete chapter
-  await runAsync(chapter.deleteOne({ _id: chapter._id }));
+  if (err3) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
 
   return responseMsg(res, {
     status: 200,
     error: false,
-    message: "Successfully deleted chapter",
+    msg: "Successfully deleted the chapter",
   });
-}
+};
