6327bd606cd3eab598138b9f8758e0912e8874c1
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:17:17 +0530

    refactor and document expertise middleware

--- backend/src/middlewares/expertise.ts
+++ backend/src/middlewares/expertise.ts
@@ -1,23 +1,17 @@
-import { NextFunction, Request, Response } from "express";
 import Expertise, { ExpertiseDocument } from "../models/expertise";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
 /**
- * Get single doc by id
+ * Get expertise (if exists) and set it to req.expertise
+ *
+ * @params
+ * id: expertise mongodb id
  */
-export async function getExpertiseById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  expertiseId: string
-) {
-  const [data, err] = await runAsync(Expertise.findById(expertiseId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Expertise doesn't exists" });
-
-  const expertise: ExpertiseDocument = data;
-  req.expertise = expertise;
-
+export const getExpertiseById: IdMiddleware = async (req, res, next, id) => {
+  const [data, err] = await runAsync(Expertise.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data) return responseMsg(res, { msg: "Expertise doesn't exists" });
+  const exp: ExpertiseDocument = data;
+  req.expertise = exp;
   next();
-}
+};
