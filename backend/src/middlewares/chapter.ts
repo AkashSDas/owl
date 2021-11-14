67f003bca395e4c9721d3e60a0e59afcc0303639
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:28:10 +0530

    refactor and document chapter middleware

--- backend/src/middlewares/chapter.ts
+++ backend/src/middlewares/chapter.ts
@@ -1,23 +1,17 @@
-import { NextFunction, Request, Response } from "express";
 import Chapter, { ChapterDocument } from "../models/chapter";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
 /**
- * Get single doc by id
+ * Get chapter (if exists) and set it to req.chapter
+ *
+ * @params
+ * id: chapter mongodb id
  */
-export async function getChapterById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  chapterId: string
-) {
-  const [data, err] = await runAsync(Chapter.findById(chapterId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Chapter doesn't exists" });
-
+export const getChapterById: IdMiddleware = async (req, res, next, id) => {
+  const [data, err] = await runAsync(Chapter.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data) return responseMsg(res, { msg: "Chapter doesn't exists" });
   const chapter: ChapterDocument = data;
   req.chapter = chapter;
-
   next();
-}
+};
