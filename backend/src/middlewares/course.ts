4980920a6dd30d568e27a18d83f051c54260da9b
AkashSDas <aakashdas368@gmail.com>
2021-11-14 19:28:18 +0530

    refactor and document course middleware

--- backend/src/middlewares/course.ts
+++ backend/src/middlewares/course.ts
@@ -1,23 +1,20 @@
-import { NextFunction, Request, Response } from "express";
 import Course, { CourseDocument } from "../models/course";
-import { responseMsg, runAsync } from "../utils";
+import { IdMiddleware, responseMsg, responseMsgs, runAsync } from "../utils";
 
 /**
  * Get single doc by id
  */
-export async function getCourseById(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  courseId: string
-) {
-  const [data, err] = await runAsync(Course.findById(courseId).exec());
-  if (err)
-    return responseMsg(res, { status: 400, message: "Something went wrong, Please try again" });
-  if (!data) return responseMsg(res, { status: 400, message: "Course doesn't exists" });
-
+/**
+ * Get course (if exists) and set it to req.course
+ *
+ * @params
+ * id: course mongodb id
+ */
+export const getCourseById: IdMiddleware = async (req, res, next, id) => {
+  const [data, err] = await runAsync(Course.findOne({ _id: id }).exec());
+  if (err) return responseMsg(res, { msg: responseMsgs.WENT_WRONG });
+  else if (!data) return responseMsg(res, { msg: "Course doesn't exists" });
   const course: CourseDocument = data;
   req.course = course;
-
   next();
-}
+};
