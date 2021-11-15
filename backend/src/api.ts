b475defcfb22898e62ef56ee4ca3ee67d9f2ae18
AkashSDas <aakashdas368@gmail.com>
2021-11-15 16:20:41 +0530

    rm course chapters router in api.ts

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -6,7 +6,6 @@ import { router as qualificationRouter } from "./routes/qualification";
 import { router as expertiseRouter } from "./routes/expertise";
 import { router as courseLevelRouter } from "./routes/course_level";
 import { router as courseRouter } from "./routes/course";
-import { router as courseChaptersRouter } from "./routes/course_chapters";
 import { router as chapterRouter } from "./routes/chapter";
 import { router as lessonRouter } from "./routes/lesson";
 
@@ -27,6 +26,5 @@ app.use("/api/qualification", qualificationRouter);
 app.use("/api/expertise", expertiseRouter);
 app.use("/api/course-level", courseLevelRouter);
 app.use("/api/course", courseRouter);
-app.use("/api/course-chapters", courseChaptersRouter);
 app.use("/api/chapter", chapterRouter);
 app.use("/api/lesson", lessonRouter);
