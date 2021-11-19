77cadde71d34aa9f6ad95ac556a9145f5ffe4e54
AkashSDas <aakashdas368@gmail.com>
2021-11-19 23:46:05 +0530

    create teacher section in index page

--- owl/pages/index.tsx
+++ owl/pages/index.tsx
@@ -9,6 +9,7 @@ const Index = () => {
     <main>
       <HeroSection />
       <ExploreSection />
+      <TeacherSection />
     </main>
   );
 };
@@ -19,7 +20,7 @@ const Index = () => {
 
 const HeroSection = () => {
   return (
-    <section style={{ height: "350px" }} className="w-full relative my-16">
+    <section className="w-full relative my-16">
       <TagLineText />
       <EmojiCard text="⏰ Learn at your own pace" position="top-0 left-8" />
       <EmojiCard text="🚀 Learn whereever you want" position="top-0 right-8" />
@@ -50,11 +51,19 @@ const TagLineText = () => {
   );
 };
 
-const EmojiCard = ({ text, position }: { text: string; position: string }) => {
+const EmojiCard = ({
+  text,
+  position,
+  opacity = "opacity-60",
+}: {
+  text: string;
+  position: string;
+  opacity?: string;
+}) => {
   return (
     <div
       style={{ boxShadow: "0px 8px  16px rgba(0, 0, 0, 0.10)" }}
-      className={`absolute ${position} bg-grey0 rounded-full p-6 font-medium text-desktop-body-main opacity-60`}
+      className={`absolute ${position} bg-grey0 rounded-full p-6 font-medium text-desktop-body-main ${opacity}`}
     >
       {text}
     </div>
@@ -67,7 +76,7 @@ const EmojiCard = ({ text, position }: { text: string; position: string }) => {
 
 const ExploreSection = () => {
   return (
-    <section className="w-full relative my-16 h-screen bg-grey5 py-8">
+    <section className="w-full relative h-screen bg-grey5 py-8">
       <h1 className="text-grey0 text-desktop-h1 w-full text-center">
         <span className="text-purple">Explore</span> your curiosity
       </h1>
@@ -103,14 +112,16 @@ const ExploreSection = () => {
 const EmojiDarkCard = ({
   text,
   position,
+  opacity = "opcaity-60",
 }: {
   text: string;
   position: string;
+  opacity?: string;
 }) => {
   return (
     <div
       style={{ boxShadow: "0px 8px  16px rgba(0, 0, 0, 0.25)" }}
-      className={`absolute ${position} bg-grey4 text-grey0 rounded-full p-4 font-medium text-desktop-body-main opacity-60`}
+      className={`absolute ${position} bg-grey4 text-grey0 rounded-full p-4 font-medium text-desktop-body-main ${opacity}`}
     >
       {text}
     </div>
@@ -120,5 +131,82 @@ const EmojiDarkCard = ({
 /**
  * Section 3
  */
+
+const TeacherSection = () => {
+  return (
+    <section className="w-full relative h-screen bg-primary py-8">
+      <h1 className="text-grey4 text-desktop-h1 w-full text-center">
+        Learn from the <span className="text-purple">best</span>
+      </h1>
+
+      <div>
+        <TeacherProfilePic position="top-40 left-48" size="100px" />
+        <TeacherProfilePic position="top-40 right-48" size="100px" />
+        <TeacherProfilePic position="bottom-24 right-24" size="125px" />
+        <TeacherProfilePic position="bottom-24 left-24" size="125px" />
+      </div>
+
+      <div className="w-full flex justify-center absolute">
+        <TeacherProfilePic
+          position="top-12"
+          opacity="opacity-100"
+          size="200px"
+        />
+        <EmojiCard
+          text="😎 UI/UX Designer"
+          opacity="opcaity-100"
+          position="left-96 top-6"
+        />
+        <EmojiCard
+          text="🚨 Motion Designing"
+          opacity="opcaity-100"
+          position="right-80 top-32"
+        />
+        <EmojiCard
+          text="🌈 Figma"
+          opacity="opcaity-100"
+          position="left-96 top-48"
+        />
+      </div>
+
+      <div
+        style={{ lineHeight: "135%" }}
+        className="flex flex-col justify-center items-center absolute -bottom-6 left-0 right-0 text-desktop-h3 font-medium text-grey4"
+      >
+        <span>Learn from the people who leading the </span>
+        <span>
+          industry, who are creating new things,
+          <span className="text-grey5 font-bold italic"> people </span>
+        </span>
+        <span className="mb-6">
+          <span className="text-grey5 font-bold italic">
+            who teach the best
+          </span>
+        </span>
+      </div>
+    </section>
+  );
+};
+
+const TeacherProfilePic = ({
+  position,
+  size,
+  opacity = "opacity-60",
+  src = "https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+}) => {
+  return (
+    <img
+      style={{
+        height: size,
+        width: size,
+        border: "3px solid black",
+        filter: "grayscale(1)",
+      }}
+      className={`rounded-full object-cover absolute ${position} ${opacity}`}
+      src={src}
+      alt=""
+    />
+  );
+};
 
 export default Index;
