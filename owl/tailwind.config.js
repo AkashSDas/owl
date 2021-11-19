e079f370db965f2ecd488fbb8d1f8077021dc822
AkashSDas <aakashdas368@gmail.com>
2021-11-19 16:06:01 +0530

    create navbar

--- owl/tailwind.config.js
+++ owl/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       grey2: "hsla(0, 0%, 0%, 0.2)",
       grey3: "hsla(0, 0%, 0%, 0.6)",
       grey4: "hsla(0, 0%, 19%, 1)",
-      errro: "hsla(360, 100%, 59%, 1)",
+      error: "hsla(360, 100%, 59%, 1)",
       success: "hsla(104, 66%, 65%, 1)",
       purple: {
         DEFAULT: "hsla(265, 100%, 59%, 1)",
@@ -37,33 +37,33 @@ module.exports = {
       // So here the font size in rem * 10 = font size in px
 
       // Desktop
-      "desktop-h1": ["6rem"],
-      "desktop-h2": ["5rem"],
-      "desktop-h3": ["4rem"],
-      "desktop-h4": ["3rem"],
-      "desktop-body-intro": ["2.4rem", "140%"],
-      "desktop-body-main": ["2rem", "140%"],
+      "desktop-h1": ["3.75rem"],
+      "desktop-h2": ["3.125rem"],
+      "desktop-h3": ["2.5rem"],
+      "desktop-h4": ["1.875rem"],
+      "desktop-body-intro": ["1.5rem", "140%"],
+      "desktop-body-main": ["1.25rem", "140%"],
 
       // For tablet size take one level lower font size
       // of desktop of which you are using like (if h1 is needed then
       // take h2 of desktop)
 
       // Mobile
-      "mobile-h1": ["3.4rem"],
-      "mobile-h2": ["3.2rem"],
-      "mobile-h3": ["2.8rem"],
-      "mobile-h4": ["2.4rem"],
-      "mobile-body-intro": ["2rem", "140%"],
-      "mobile-body-main": ["1.7rem", "140%"],
+      "mobile-h1": ["2.125rem"],
+      "mobile-h2": ["2rem"],
+      "mobile-h3": ["1.75rem"],
+      "mobile-h4": ["1.5rem"],
+      "mobile-body-intro": ["1.25rem", "140%"],
+      "mobile-body-main": ["1.063rem", "140%"],
 
       // Font size that are common in all viewports
-      medium: ["1.7rem"],
-      caption: ["1.5rem"],
-      small: ["1.3rem"],
+      medium: ["1.063rem"],
+      caption: ["0.938rem"],
+      small: ["0.813rem"],
 
       // Button
-      "btn-20": ["2rem"],
-      "btn-17": ["1.7rem"],
+      "btn-20": ["1.25rem"],
+      "btn-17": ["1.063rem"],
     },
   },
   variants: {
