7bd32354bcb1393c4ecb78edfce2333667a374c6
AkashSDas <aakashdas368@gmail.com>
2021-11-21 19:51:44 +0530

    create signup validations

---
+++ owl/lib/validation.ts
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+
+export const signupValidationSchema = Yup.object().shape({
+  username: Yup.string()
+    .required("Username is required")
+    .min(6, "Username should be atleast 6 characters long"),
+  email: Yup.string()
+    .required("Email is required")
+    .email("Email has wrong format"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password should be atleast 6 characters long"),
+  dateOfBirth: Yup.date().required("Date of birth is required"),
+});
