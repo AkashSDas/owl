3ba79fae4365ea75b5111542d6d6e427bad0dbf0
AkashSDas <aakashdas368@gmail.com>
2021-11-21 19:52:27 +0530

    add validations in all signup form

--- owl/components/signup/form.tsx
+++ owl/components/signup/form.tsx
@@ -3,6 +3,7 @@ import { MouseEventHandler, useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { ISignupForm, SignupContext } from "../../lib/context/auth";
 import { signup } from "../../lib/helpers/auth";
+import { signupValidationSchema } from "../../lib/validation";
 import { UsernameField, EmailField, PasswordField, DateField } from "./fields";
 
 export const SignupForm = () => {
@@ -23,10 +24,30 @@ export const SignupForm = () => {
   };
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-      {({ values, handleSubmit, handleChange }) => (
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      validationSchema={signupValidationSchema}
+    >
+      {({
+        values,
+        handleSubmit,
+        handleChange,
+        errors,
+        touched,
+        handleBlur,
+      }) => (
         <SignupContext.Provider
-          value={{ values, handleSubmit, handleChange, loading, setLoading }}
+          value={{
+            values,
+            handleSubmit,
+            handleChange,
+            loading,
+            setLoading,
+            errors,
+            touched,
+            handleBlur,
+          }}
         >
           <form className="mt-8 flex flex-col items-start w-2/4 space-y-5">
             <UsernameField />
