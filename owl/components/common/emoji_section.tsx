c65bbbf447958cf1d357a5b5c5533f6d24e93343
AkashSDas <aakashdas368@gmail.com>
2021-11-21 11:33:11 +0530

    create emoji section

---
+++ owl/components/common/emoji_section.tsx
@@ -0,0 +1,12 @@
+export const EmojiSection = () => {
+  return (
+    <div style={{ zIndex: -1 }} className="absolute w-full">
+      <span className="text-desktop-h4 absolute top-10 left-32">🎸</span>
+      <span className="text-desktop-h4 absolute top-48 left-48">🔬</span>
+      <span className="text-desktop-h4 absolute top-96 left-40">📖</span>
+      <span className="text-desktop-h4 absolute top-10 right-32">🔭</span>
+      <span className="text-desktop-h4 absolute top-48 right-48">👓</span>
+      <span className="text-desktop-h4 absolute top-96 right-40">🎥</span>
+    </div>
+  );
+};
