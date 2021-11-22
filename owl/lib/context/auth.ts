c33ae059eb74ac26606ccc4fd51ff6308689a83c
AkashSDas <aakashdas368@gmail.com>
2021-11-22 11:47:56 +0530

    create login context

--- owl/lib/context/auth.ts
+++ owl/lib/context/auth.ts
@@ -1,6 +1,10 @@
 import { FormikErrors, FormikTouched } from "formik";
 import { ChangeEventHandler, createContext, FocusEventHandler } from "react";
 
+/**
+ * Signup context
+ */
+
 export interface ISignupForm {
   username: string;
   email: string;
@@ -20,3 +24,25 @@ interface ISignupContext {
 }
 
 export const SignupContext = createContext<ISignupContext>(null);
+
+/**
+ * Login context
+ */
+
+export interface ILoginForm {
+  email: string;
+  password: string;
+}
+
+interface ILoginContext {
+  loading: boolean;
+  setLoading: Function;
+  values: ILoginForm;
+  handleSubmit: Function;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+  errors: FormikErrors<ISignupForm>;
+  touched: FormikTouched<ISignupForm>;
+  handleBlur: FocusEventHandler<HTMLInputElement>;
+}
+
+export const LoginContext = createContext<ILoginContext>(null);
