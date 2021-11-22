d69cdcd4a74a1d0fff074eb73bc46559ff105bbe
AkashSDas <aakashdas368@gmail.com>
2021-11-22 12:01:06 +0530

    create save user to local storage and is authenticated

--- owl/lib/helpers/auth.ts
+++ owl/lib/helpers/auth.ts
@@ -24,3 +24,24 @@ export const login = async (data: ILoginForm) => {
   if (err) return [null, err.response.data];
   return [result.data, null];
 };
+
+/**
+ * Save user in local storage
+ */
+export const saveUserToLocalStorage = (data: any, next: Function) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+/**
+ * Check user data if exists in local storage if yes then send it
+ * else send null
+ */
+export function isAuthenticated() {
+  if (typeof window === "undefined") return null;
+  if (localStorage.getItem("jwt"))
+    return JSON.parse(localStorage.getItem("jwt"));
+  return null;
+}
