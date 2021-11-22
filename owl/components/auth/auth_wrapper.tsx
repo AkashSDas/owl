644a00c5083da3c42203b2c04807ec4ca1eac8d1
AkashSDas <aakashdas368@gmail.com>
2021-11-22 14:26:50 +0530

    create auth wrapper component

---
+++ owl/components/auth/auth_wrapper.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../lib/context/user";
+import { isAuthenticated } from "../../lib/helpers/auth";
+
+/**
+ * This will set the user when the app is loaded
+ */
+export const AuthWrapper = ({ children }: { children: JSX.Element }) => {
+  const { setUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const auth = isAuthenticated();
+    if (auth)
+      setUser({
+        token: auth.token,
+        data: {
+          createdAt: auth.user.createdAt,
+          dateOfBirth: auth.user.dateOfBirth,
+          email: auth.user.email,
+          profilePicURL: auth.user.profilePicURL,
+          roles: auth.user.roles,
+          updatedAt: auth.user.updatedAt,
+          username: auth.user.username,
+        },
+      });
+  }, []);
+
+  return <div>{children}</div>;
+};
