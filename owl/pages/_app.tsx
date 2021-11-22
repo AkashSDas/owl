0f183710e97a4d6945ae4b0cbde1b4c7c9c3baa2
AkashSDas <aakashdas368@gmail.com>
2021-11-22 14:28:05 +0530

    wrap my app with auth provider and auth wrapper in _app.tsx

--- owl/pages/_app.tsx
+++ owl/pages/_app.tsx
@@ -6,6 +6,8 @@ import { IconlyProvider } from "react-iconly";
 import { Navbar } from "../components/common/navbar";
 import { CursorProvider } from "../components/cursor/cursor_provider";
 import { SkewScrollingAnimation } from "../components/skew_scrolling/animation";
+import { AuthWrapper } from "../components/auth/auth_wrapper";
+import { AuthProvider } from "../components/auth/auth_provider";
 
 /**
  * @remarks
@@ -13,19 +15,23 @@ import { SkewScrollingAnimation } from "../components/skew_scrolling/animation";
  */
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <CursorProvider>
-      <IconlyProvider
-        set="light"
-        size="medium"
-        primaryColor="hsla(0, 0%, 0%, 0.5)"
-      >
-        <Navbar />
-        <SkewScrollingAnimation>
-          <Component {...pageProps} />
-        </SkewScrollingAnimation>
-        <Toaster />
-      </IconlyProvider>
-    </CursorProvider>
+    <AuthProvider>
+      <AuthWrapper>
+        <CursorProvider>
+          <IconlyProvider
+            set="light"
+            size="medium"
+            primaryColor="hsla(0, 0%, 0%, 0.5)"
+          >
+            <Navbar />
+            <SkewScrollingAnimation>
+              <Component {...pageProps} />
+            </SkewScrollingAnimation>
+            <Toaster />
+          </IconlyProvider>
+        </CursorProvider>
+      </AuthWrapper>
+    </AuthProvider>
   );
 };
 
