026a15a50ba74116a29c80502c27478486f77aca
AkashSDas <aakashdas368@gmail.com>
2021-11-22 17:09:20 +0530

    mv teacher context to auth context file

--- owl/lib/context/auth.ts
+++ owl/lib/context/auth.ts
@@ -46,3 +46,27 @@ interface ILoginContext {
 }
 
 export const LoginContext = createContext<ILoginContext>(null);
+
+/**
+ * Teacher signup
+ */
+
+export interface ITeacherSignupForm {
+  bio: string;
+  yearsOfExperience: number;
+  qualifications: string[];
+  expertise: string[];
+}
+
+interface ITeacherSignupContext {
+  loading: boolean;
+  setLoading: Function;
+  values: ITeacherSignupForm;
+  handleSubmit: Function;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+  errors: FormikErrors<ITeacherSignupForm>;
+  touched: FormikTouched<ITeacherSignupForm>;
+  handleBlur: FocusEventHandler<HTMLInputElement>;
+}
+
+export const TeacherSignupContext = createContext<ITeacherSignupContext>(null);
