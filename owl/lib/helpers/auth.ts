1d9a36d4d6c85f9f13c5f3c9b044d3b7737d84a4
AkashSDas <aakashdas368@gmail.com>
2021-11-22 11:48:20 +0530

    create login api function

--- owl/lib/helpers/auth.ts
+++ owl/lib/helpers/auth.ts
@@ -1,4 +1,4 @@
-import { ISignupForm } from "../context/auth";
+import { ILoginForm, ISignupForm } from "../context/auth";
 import { fetchFromAPI } from "./base";
 
 /**
@@ -6,6 +6,18 @@ import { fetchFromAPI } from "./base";
  */
 export const signup = async (data: ISignupForm) => {
   const [result, err] = await fetchFromAPI("/auth/signup", {
+    method: "POST",
+    data,
+  });
+  if (err) return [null, err.response.data];
+  return [result.data, null];
+};
+
+/**
+ * User login
+ */
+export const login = async (data: ILoginForm) => {
+  const [result, err] = await fetchFromAPI("/auth/login", {
     method: "POST",
     data,
   });
