945ed4711628061bee957b300a5c9b489e1c882c
AkashSDas <aakashdas368@gmail.com>
2021-11-23 17:12:26 +0530

    create emoji section for sidebar

--- owl/components/common/emoji_section.tsx
+++ owl/components/common/emoji_section.tsx
@@ -10,3 +10,16 @@ export const EmojiSection = () => {
     </div>
   );
 };
+
+export const EmojiSectionForSidebar = () => {
+  return (
+    <div style={{ zIndex: -1 }} className="absolute w-full">
+      <span className="text-desktop-h4 absolute top-10 left-8">🎸</span>
+      <span className="text-desktop-h4 absolute top-48 left-5">🔬</span>
+      <span className="text-desktop-h4 absolute top-96 left-9">📖</span>
+      <span className="text-desktop-h4 absolute top-10 right-8">🔭</span>
+      <span className="text-desktop-h4 absolute top-48 right-5">👓</span>
+      <span className="text-desktop-h4 absolute top-96 right-9">🎥</span>
+    </div>
+  );
+};
