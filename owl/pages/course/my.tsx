107cfec412c4ab3af7fc805493ab45540309a19e
AkashSDas <aakashdas368@gmail.com>
2021-11-23 18:06:24 +0530

    create my courses page

---
+++ owl/pages/course/my.tsx
@@ -0,0 +1,11 @@
+const MyCourses = () => {
+  return (
+    <main style={{ marginLeft: "250px" }} className="px-2">
+      <section className="mt-8 w-full flex flex-col items-center relative h-screen">
+        <h1 className="text-desktop-h4">My Courses</h1>
+      </section>
+    </main>
+  );
+};
+
+export default MyCourses;
