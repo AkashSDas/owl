1beb234076dca609d60bad81a4f8460802f89a41
AkashSDas <aakashdas368@gmail.com>
2021-11-25 19:54:55 +0530

    add remarks in use course id for sidebar hook

--- owl/lib/hooks/sidebar.ts
+++ owl/lib/hooks/sidebar.ts
@@ -3,6 +3,7 @@ import { useContext, useEffect } from "react";
 import { CourseEditorSidebarContext } from "../context/sidebar";
 
 /**
+ * @remarks
  * Use this in every page where you want CourseEditorSidebarContext
  * to be available
  */
