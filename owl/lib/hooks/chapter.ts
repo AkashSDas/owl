6002bb8336e52202718c0456ce12d87c9b382e99
AkashSDas <aakashdas368@gmail.com>
2021-11-26 00:16:48 +0530

    create use chapters of course hook

---
+++ owl/lib/hooks/chapter.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { getAllChaptersOfCourse } from "../helpers/chapter";
+
+export const useChaptersOfCourse = (courseId: string) => {
+  const [loading, setLoading] = useState(false);
+  const [chapters, setChapters] = useState([]);
+
+  const apiCall = async () => {
+    if (courseId) {
+      setLoading(true);
+      const [data, err] = await getAllChaptersOfCourse(courseId);
+      if (err) toast(err.msg, { icon: "❌" });
+      else setChapters(data.data.chapters);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, [courseId]);
+
+  return { loading, chapters };
+};
