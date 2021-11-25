4da3dbbbe335a61d8192a1918fcbb8d55c601018
AkashSDas <aakashdas368@gmail.com>
2021-11-25 10:07:36 +0530

    create delete course helper function

--- owl/lib/helpers/course.ts
+++ owl/lib/helpers/course.ts
@@ -32,6 +32,21 @@ export const getUserAllCourses = async (
 
   const [result, err] = await fetchFromAPI(url, { method: "GET", token });
   if (err) return [null, err.response.data];
-  console.log(result);
+  return [result.data, null];
+};
+
+/**
+ * Delete a course
+ */
+export const deleteCourse = async (
+  courseId: string,
+  userId: string,
+  token: string
+) => {
+  const [result, err] = await fetchFromAPI(`/course/${userId}/${courseId}`, {
+    method: "DELETE",
+    token,
+  });
+  if (err) return [null, err.response.data];
   return [result.data, null];
 };
