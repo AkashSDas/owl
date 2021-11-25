bd698149f61c24becf386e99d1bce7677fbeb768
AkashSDas <aakashdas368@gmail.com>
2021-11-25 14:42:15 +0530

    create helper functions for getting course public data and updating course public data

--- owl/lib/helpers/course.ts
+++ owl/lib/helpers/course.ts
@@ -1,5 +1,6 @@
 import { ICourseCreateForm } from "../context/course";
-import { fetchFromAPI } from "./base";
+import { runAsync } from "../utils";
+import { axiosBaseInstance, fetchFromAPI } from "./base";
 
 /**
  * Create a course
@@ -47,6 +48,48 @@ export const deleteCourse = async (
     method: "DELETE",
     token,
   });
+  if (err) return [null, err.response.data];
+  return [result.data, null];
+};
+
+/**
+ * Get course data for updating course
+ */
+export const getCoursePublicData = async (courseId: string) => {
+  const [result, err] = await fetchFromAPI(`/course/${courseId}/public`, {
+    method: "GET",
+  });
+  if (err) return [null, err.response.data];
+  return [result.data, null];
+};
+
+/**
+ * Update course public data
+ */
+export const updateCoursePublicData = async (
+  courseId: string,
+  data: any,
+  userId: string,
+  token: string
+) => {
+  // Formdata
+  const formData = new FormData();
+  for (const field in data) {
+    formData.append(field, data[field]);
+  }
+
+  const api = axiosBaseInstance();
+
+  const [result, err] = await runAsync(
+    api(`/course/${userId}/${courseId}`, {
+      method: "PUT",
+      data: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  );
+
   if (err) return [null, err.response.data];
   return [result.data, null];
 };
