e76abc376d8899703adc93c6707a4efc2e6b5a05
AkashSDas <aakashdas368@gmail.com>
2021-11-25 14:41:48 +0530

    create context for updating course

--- owl/lib/context/course.ts
+++ owl/lib/context/course.ts
@@ -1,5 +1,10 @@
 import { FormikErrors, FormikTouched } from "formik";
-import { ChangeEventHandler, createContext, FocusEventHandler } from "react";
+import {
+  ChangeEventHandler,
+  createContext,
+  Dispatch,
+  FocusEventHandler,
+} from "react";
 
 export interface ICourseCreateForm {
   name: string;
@@ -20,3 +25,30 @@ interface ICourseCreateContext {
 }
 
 export const CourseCreateContext = createContext<ICourseCreateContext>(null);
+
+/**
+ * Update course
+ */
+
+export interface ICourseUpdateForm {
+  name: string;
+  description: string;
+  level: string;
+  price: number;
+}
+
+interface ICourseUpdateContext {
+  loading: boolean;
+  setLoading: Function;
+  values: ICourseUpdateForm;
+  handleSubmit: Function;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+  errors: FormikErrors<ICourseUpdateForm>;
+  touched: FormikTouched<ICourseUpdateForm>;
+  handleBlur: FocusEventHandler<HTMLInputElement>;
+  coverImgURL: string;
+  coverImgFile: File;
+  setCoverImgFile: Dispatch<File>;
+}
+
+export const CourseUpdateContext = createContext<ICourseUpdateContext>(null);
