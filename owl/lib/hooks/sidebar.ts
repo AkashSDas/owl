5157e131c55e9771db714979d6f90553053295df
AkashSDas <aakashdas368@gmail.com>
2021-11-25 19:54:47 +0530

    add remarks in use course id for sidebar hook

--- owl/lib/hooks/sidebar.ts
+++ owl/lib/hooks/sidebar.ts
@@ -2,6 +2,10 @@ import { useRouter } from "next/dist/client/router";
 import { useContext, useEffect } from "react";
 import { CourseEditorSidebarContext } from "../context/sidebar";
 
+/**
+ * Use this in every page where you want CourseEditorSidebarContext
+ * to be available
+ */
 export const useCourseIdForSidebar = () => {
   const router = useRouter();
   const { courseId } = router.query;
