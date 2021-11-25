278404c24123e965e0a48b41dc79e0093b2cf1d6
AkashSDas <aakashdas368@gmail.com>
2021-11-25 19:53:46 +0530

    refactor sidebar components and use CourseEditorSidebarContext

--- owl/components/common/sidebar.tsx
+++ owl/components/common/sidebar.tsx
@@ -1,81 +1,126 @@
 import { useRouter } from "next/dist/client/router";
-import { Document, Play } from "react-iconly";
+import { Document, Folder, Play } from "react-iconly";
 import btnStyle from "../../styles/components/sidebar/SidebarButton.module.scss";
 import sidebarStyle from "../../styles/components/sidebar/Sidebar.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { CourseEditorSidebarContext } from "../../lib/context/sidebar";
 
 export const BaseSidebar = () => {
   const router = useRouter();
 
-  const routes = ["/course/create", "/course/my"];
-  const patterns = [new RegExp("/course/update/*")];
-  if (
-    routes.filter(
-      (r) => r === router.route || patterns.map((p) => p.exec(r)).length !== 0
-    ).length !== 0
-  ) {
-    return (
-      <div
-        style={{ width: "250px", marginTop: "72px" }}
-        className={`bg-grey1 h-screen fixed flex flex-col p-4 ${sidebarStyle["sidebar"]}`}
-      >
-        <SidebarButton
-          icon={
-            <Document
-              primaryColor={
-                router.route !== "/course/my" ? "hsla(0, 0%, 19%, 1)" : "white"
-              }
-            />
-          }
-          text="My Courses"
-          route="/course/my"
-        />
-        <SidebarButton
-          icon={
-            <Play
-              primaryColor={
-                router.route !== "/course/create"
-                  ? "hsla(0, 0%, 19%, 1)"
-                  : "white"
-              }
-            />
-          }
-          text="Create a course"
-          route="/course/create"
-        />
-      </div>
-    );
-  }
+  const routes1 = ["/course/create", "/course/my", "/course/[courseId]/update"];
+  const routes2 = [
+    "/course/[courseId]/chapter",
+    "/course/[courseId]/chapter/create",
+  ];
+
+  const isSidebar1 = routes1.filter((r) => r === router.route).length !== 0;
+  const isSidebar2 = routes2.filter((r) => r === router.route).length !== 0;
+
+  if (isSidebar1) return <Sidebar1 />;
+  if (isSidebar2) return <Sidebar2 />;
 
   return null;
 };
 
-const SidebarButton = ({
-  route,
-  icon,
-  text,
-}: {
+const Sidebar1 = () => {
+  const router = useRouter();
+
+  return (
+    <div
+      style={{ width: "250px", marginTop: "72px" }}
+      className={`bg-grey1 h-screen fixed flex flex-col p-4 ${sidebarStyle["sidebar"]}`}
+    >
+      <SidebarButton
+        icon={
+          <Document
+            primaryColor={
+              router.route !== "/course/my" ? "hsla(0, 0%, 19%, 1)" : "white"
+            }
+          />
+        }
+        text="My Courses"
+        route="/course/my"
+        routePattern="/course/my"
+      />
+      <SidebarButton
+        icon={
+          <Play
+            primaryColor={
+              router.route !== "/course/create"
+                ? "hsla(0, 0%, 19%, 1)"
+                : "white"
+            }
+          />
+        }
+        text="Create a course"
+        route="/course/create"
+        routePattern="/course/create"
+      />
+    </div>
+  );
+};
+
+const Sidebar2 = () => {
+  const router = useRouter();
+  const { sidebar } = useContext(CourseEditorSidebarContext);
+
+  return (
+    <div
+      style={{ width: "250px", marginTop: "72px" }}
+      className={`bg-grey1 h-screen fixed flex flex-col p-4 ${sidebarStyle["sidebar"]}`}
+    >
+      <SidebarButton
+        icon={<Play primaryColor={false ? "white" : "hsla(0, 0%, 19%, 1)"} />}
+        text="Course Look"
+        routePattern="/no-match"
+        route="#"
+      />
+      <SidebarButton
+        icon={
+          <Folder
+            primaryColor={
+              router.route !== "/course/[courseId]/chapter"
+                ? "hsla(0, 0%, 19%, 1)"
+                : "white"
+            }
+          />
+        }
+        text="Chapters"
+        route={sidebar.courseId ? `/course/${sidebar.courseId}/chapter` : "#"}
+        routePattern={"/course/[courseId]/chapter"}
+      />
+    </div>
+  );
+};
+
+interface SidebarButtonProps {
+  routePattern: string;
   route: string;
   icon: JSX.Element;
   text: string;
-}) => {
+}
+
+const SidebarButton = (props: SidebarButtonProps) => {
   const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (router.route === props.routePattern) {
+      setIsActive(true);
+    }
+  }, [router.route]);
 
   return (
     <button
       className={`p-4 rounded-2xl flex justify-start items-center space-x-2 ${
-        router.route !== route ? "bg-grey1" : "bg-secondary"
-      } ${
-        router.route !== route ? btnStyle["sidebar-btn"] : ""
-      } cursor-pointer`}
-      onClick={() => router.push(route)}
+        !isActive ? "bg-grey1" : "bg-secondary"
+      } ${!isActive ? btnStyle["sidebar-btn"] : ""} cursor-pointer`}
+      onClick={() => router.push(props.route)}
     >
-      <div>{icon}</div>
-      <div
-        className={`${
-          router.route !== route ? "text-grey4" : "text-grey0"
-        } font-medium`}
-      >
-        {text}
+      <div>{props.icon}</div>
+      <div className={`${!isActive ? "text-grey4" : "text-grey0"} font-medium`}>
+        {props.text}
       </div>
     </button>
   );
