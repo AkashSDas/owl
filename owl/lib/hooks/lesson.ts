2ea71900135a5cf936a98b6c722f38c1cc5829c8
AkashSDas <aakashdas368@gmail.com>
2021-11-26 18:21:25 +0530

    create use lesson hook

--- owl/lib/hooks/lesson.ts
+++ owl/lib/hooks/lesson.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { getAllLessonsOfChapter } from "../helpers/lesson";
+import { getAllLessonsOfChapter, getLesson } from "../helpers/lesson";
 
 export const useLessonsOfChapter = (courseId: string, chapterId: string) => {
   const [loading, setLoading] = useState(false);
@@ -25,4 +25,29 @@ export const useLessonsOfChapter = (courseId: string, chapterId: string) => {
   }, [courseId, chapterId]);
 
   return { loading, lessons, setLessons, chapterInfo };
+};
+
+export const useLesson = (
+  courseId: string,
+  chapterId: string,
+  lessonId: string
+) => {
+  const [loading, setLoading] = useState(false);
+  const [lesson, setLesson] = useState(null);
+
+  const apiCall = async () => {
+    if (courseId && chapterId && lessonId) {
+      setLoading(true);
+      const [data, err] = await getLesson(courseId, chapterId, lessonId);
+      if (err) toast(err.msg, { icon: "❌" });
+      else setLesson(data.data.lesson);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, [courseId, chapterId, lessonId]);
+
+  return { loading, lesson };
 };
